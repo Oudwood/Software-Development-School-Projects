ea21a92afab244e408e05a286753c1f7b2740a8c
Literate Broccoli <55304061+Oudwood@users.noreply.github.com>
2021-03-02 15:05:42 -0500

    Add files via upload

---
+++ RockPaperScissors/RockPaperScissors/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RockPaperScissors
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VM vm = new VM();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+
+        private void StartOver_Click(object sender, RoutedEventArgs e)
+        {
+            vm.Clear();
+        }
+    }
+}
